0be132f8cc74aa9f00718d01168eb1c6803aed9c
TerrenceGoh <29521214+tgohblio@users.noreply.github.com>
2023-10-05 23:07:13 +0800

    test clicking a youtube link

--- basic.py
+++ basic.py
@@ -9,9 +9,26 @@ def main():
         # page.screenshot(path="yahoo.png")
         browser.close()
 
+
+def youtube_likes():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=args.display)
+        page = browser.new_page()
+        page.goto("https://www.youtube.com")
+        page.get_by_placeholder("Search").fill("Seatin man of legends")
+        page.get_by_role("search-icon-legacy").click()
+        page.get_by_title("October Paragon Gauntlet 100% - Morbius & Werewolf Boss Fights - Marvel Contest of Champions").click()
+        page.pause()
+        # page.locator("#title").locator("USE THESE BUFFS TO CHEESE!").click()
+        # page.screenshot(path="yahoo.png")
+        # browser.close()
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("simple playwright script")
     parser.add_argument('-d', '--display', action='store_false', help='launch the browser when executing the script')
     
     args = parser.parse_args()
-    main()
+    # main()
+    youtube_likes()
+
